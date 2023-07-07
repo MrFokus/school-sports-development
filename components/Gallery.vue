bcb11aab91eaa6e2731a80fe0b2dd4f592ab6e17
GlubokovGV <golubkovgv@admlr.lipetsk.ru>
2023-07-08 02:27:42 +0300

    feat: add new video for main page and add photo

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="gallery">
     <div v-for="s in source" class="video">
-      <iframe class="frame" :src="s" :width="width" :height="height" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+      <iframe v-if="s.type==='video'" class="frame" :src="s.src" :width="width" :height="height" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+      <img class="frame" :style="`width:${width}px; object-fit:cover;`" v-else :src="s.src" alt="">
     </div>
   </div>
 </template>
@@ -12,7 +13,32 @@ export default {
     return{
       width:0,
       height:0,
-      source:['https://vk.com/video_ext.php?oid=-187206619&id=456239027&hash=10cdf71a2cab19a3&hd=2','https://vk.com/video_ext.php?oid=-187206619&id=456239020&hash=7b89e6f41345b686&hd=3','https://vk.com/video_ext.php?oid=-187206619&id=456239023&hash=22ef103f6d90dabc&hd=1','https://vk.com/video_ext.php?oid=-187206619&id=456239031&hash=a89e69b8786fdb3d&hd=2']
+      source:[
+        {
+          src:'https://sun1-47.userapi.com/impg/_48hsTgtgPV-RvLXzU04MaVJHMFDTPKOueMZbg/pOB2bT0320Q.jpg?size=2560x1920&quality=95&sign=e042b0368894952cec4c08686957f9e3&type=album',
+          type:'img',
+        },
+        {
+          src:'https://vk.com/video_ext.php?oid=-187206619&id=456239027&hash=10cdf71a2cab19a3&hd=2',
+          type:'video',
+        },
+        {
+          src:'https://sun9-66.userapi.com/impg/WDVT8Fw06EvunOS1lUGALoo-xsKXYG0Q_oF5LQ/QzePf-3VUdw.jpg?size=2560x1920&quality=95&sign=2f2e36d05133baba7e16642fadafe1d7&type=album',
+          type:'img',
+        },
+        {
+          src:'https://vk.com/video_ext.php?oid=-187206619&id=456239020&hash=7b89e6f41345b686&hd=3',
+          type:'video',
+        },
+        {
+          src:'https://vk.com/video_ext.php?oid=-187206619&id=456239023&hash=22ef103f6d90dabc&hd=1',
+          type:'video',
+        },
+        {
+          src:'https://vk.com/video_ext.php?oid=-187206619&id=456239031&hash=a89e69b8786fdb3d&hd=2',
+          type:'video',
+        }
+      ]
     }
   },
   methods:{
