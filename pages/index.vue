4aaf1b5eadd8bb30df2c62bb3b2c9e69a25237bc
GlubokovGV <golubkovgv@admlr.lipetsk.ru>
2023-08-03 19:22:07 +0300

    feat: noscript

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,9 @@
           <button @click="$store.commit('modal/setModalActive',true)">ЗАПИСАТЬСЯ</button>
         </div>
         <div class="schedule">
+          <noscript>
           <table-schedule :schedule="schedule"/>
+          </noscript>
         </div>
       </div>
       <div class="block-gallery">
