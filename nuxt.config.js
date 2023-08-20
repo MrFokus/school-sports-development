b48df1527bc8ffbd1197c1f5986ca757fde76450
GlubokovGV <golubkovgv@admlr.lipetsk.ru>
2023-08-21 00:07:18 +0300

    feat:yandex metrica

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,31 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    script: [
+      {
+        type: "text/javascript",
+        innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+        ym(94688483, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/94688483" style="position:absolute; left:-9999px;" alt="" /></div></noscript>'`
+      }
+    ],
     htmlAttrs: {
       lang: 'ru'
     },
     meta: [
       {charset: 'utf-8'},
-      {name:"yandex-verification", content:"68b409f1c3ce78c3"},
+      {name: "yandex-verification", content: "68b409f1c3ce78c3"},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {name: "yandex-verification", content: "c0bbdba8d1761292"},
       {name: "format-detection", content: "telephone=no"},
